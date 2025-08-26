7ec8512ed3cd7599fe76f4a0c64ee0986153ad13
TheCodecOfficial <cpmct32@gmail.com>
2025-08-26 12:25:28 +0200

    update cbox example 1

--- cbox_example_1.py
+++ cbox_example_1.py
@@ -4,7 +4,6 @@ from validation_tools.validation import ValidationSuite
 val = ValidationSuite("cbox_example_1")
 
 cbox = make_cbox_scene()
-cbox.set_quality("m")
 
 val.register_scene(cbox)
 val.render()
